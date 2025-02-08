ad8f7e4818d4d95d595d5c919cc81c709c069aa9
Ma <dasklagelied@163.com>
2025-02-08 10:42:24 +0000

    "replace api, allowing both local and frontend link on render after deployment"

--- backend/src/main/java/com/example/oxo/config/WebConfig.java
+++ backend/src/main/java/com/example/oxo/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")  // 允许所有API路径
 				.allowedOrigins(
-						"https://tic-tac-toe-ingn.onrender.com",  // 替换为实际前端域名
+						"https://tic-tac-toe-1-fr16.onrender.com",  // 替换为实际前端域名
 						"http://localhost:5173"          // 本地开发环境
 				)
 				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
